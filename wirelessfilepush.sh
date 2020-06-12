9069774fa9db0e8c90bb04dc809677de5380c164
Aadi Kapur <aadikapur1@gmail.com>
2020-06-11 23:42:54 -0400

    Update README.md

---
+++ wirelessfilepush.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#download adb and connect usb before this
+PORT=5555
+
+#get USB name and IP address
+if [ ! -z "$(ls | grep ip.txt)" ];
+then
+    echo 'Using IP Address in ip.txt...'
+    IP_ADDRESS=$(cat ip.txt)
+else
+    echo 'Finding IP Address through USB...'
+    IP_ADDRESS=$(adb shell "ip a | grep 'inet ' | grep global | sed 's/ *//' \
+        | cut -d' ' -f2| sed 's/\/.*//'")
+    echo $IP_ADDRESS > ip.txt
+fi
+
+#connect wirelessly
+adb tcpip $PORT
+adb connect $IP_ADDRESS:$PORT | grep connected
+
+#we are connected wirelessly now.
+echo "Remove USB connection if its connected then press any key..."
+read -sn1 input
+read -p "Enter path of file relative to \$HOME to push: " filepath
+adb push ~/$filepath /storage/self/primary/Download/
